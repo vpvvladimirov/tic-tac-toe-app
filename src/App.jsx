d8a2b784b568eeb98b245dd062e4dde5515c245e
Vladimir <vladiv291@gmail.com>
2026-03-12 10:57:31 +0200

    Created Player component and added name change functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import Header from "./components/Header";
+import Player from "./components/Player";
+
 function App() {
   return (
-    <p>Coming soon...</p>
+    <>
+      <Header />
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player initialName="Player 1" symbol="X" />
+            <Player initialName="Player 2" symbol="O" />
+          </ol>
+          GAME BOARD
+        </div>
+        LOG
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      />
+    );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{btnCaption}</button>
+      </span>
+    </li>
+  );
+}
+
+export default Player;
